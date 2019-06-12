5db481c2459bf9ed98375e588643cb12225c7fc0
pnhung177 <pnhung177@acegik.net>
2019-06-12 17:37:04 +0700

    Refactoring source code

--- src/bridge.js
+++ src/bridge.js
@@ -5,38 +5,54 @@ var util = require('util');
 
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
+var mongojs = require('mongojs');
 
 var Devebot = require('devebot');
 var Promise = Devebot.require('bluebird');
 var lodash = Devebot.require('lodash');
-var debug = Devebot.require('debug');
-var debuglog = debug('devebot:co:mongodb:mongodbBridge');
+var pinbug = Devebot.require('pinbug');
+var debuglog = pinbug('devebot:co:mongodb:bridge');
 
-var chores = require('../utils/chores.js');
-
-var noop = function() {};
+var Helper = require('./helper');
+var chores = require('./utils/chores');
 
 var Service = function(params) {
-  debuglog.isEnabled && debuglog(' + constructor start ...');
+  debuglog.enabled && debuglog(' + constructor start ...');
 
   params = params || {};
 
   var self = this;
 
-  self.logger = self.logger || params.logger || { trace: noop, info: noop, debug: noop, warn: noop, error: noop };
+  self.logger = self.logger || params.logger || chores.emptyLogger;
 
-  var mongo_conf = params.connection_options || {};
+  var mongo_conf = params.connection_options || params.connectionOptions || {};
   var mongo_connection_string = chores.buildMongodbUrl(mongo_conf);
 
-  self.getDbObject = function(url) {
+  self.open = function(url) {
     url = url || mongo_connection_string;
     return Promise.promisify(function(done) {
-      MongoClient.connect(url, function(err, db) {
-        debuglog.isEnabled && debuglog(' - Make a connection to [%s]', url);
-        done(err, db);
+      MongoClient.connect(url, function(err, client) {
+        debuglog.enabled && debuglog(' - Make a connection to [%s]', url);
+        done(err, client);
       });
     })();
   }
+
+  var _client;
+  Object.defineProperty(self, 'client', {
+    get: function() {
+      return (_client = _client || mongojs(mongo_connection_string));
+    },
+    set: function(value) {}
+  });
+
+  var _helper;
+  Object.defineProperty(self, 'helper', {
+    get: function() {
+      return (_helper = _helper || new Helper({ client: self.client, logger: self.logger }));
+    },
+    set: function(value) {}
+  });
 
   self.getServiceInfo = function() {
     var conf = lodash.pick(mongo_conf, ['host', 'port', 'name', 'username', 'password']);
@@ -63,17 +79,9 @@ var Service = function(params) {
     }];
   };
 
-  debuglog.isEnabled && debuglog(' - constructor has finished');
+  debuglog.enabled && debuglog(' - constructor has finished');
 };
 
-Service.argumentSchema = {
-  "id": "mongodbBridge",
-  "type": "object",
-  "properties": {
-    "connection_options": {
-      "type": "object"
-    }
-  }
-};
+Service.metadata = require('./metadata');
 
 module.exports = Service;
